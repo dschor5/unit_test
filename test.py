fa7def635548c5154c40032b92861c06228dbc30
dschor <dario.schor@und.edu>
2020-12-25 21:47:28 -0500

    Changed test framework to automate code coverage

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
-import unittest
+#import unittest
 from main_app import sumInts
 
-class TestSum(unittest.TestCase):
+class TestSum:
    def test_sum(self):
-      self.assertEqual(sumInts(1, 2, 3), 6);
-      self.assertEqual(sumInts(1, 2), 3);
-
-if __name__ == '__main__':
-   unittest.main()
+      assert sumInts(1, 2, 3) == 6
+      assert sumInts(1, 2) == 3
