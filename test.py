5ab55a528dbfe7fb96c14b82e69a25c96f66c4a7
dschor <dario.schor@und.edu>
2020-12-25 22:05:28 -0500

    Fixed test

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ from main_app import sumInts
 class TestSum(unittest.TestCase):
    def test_sum(self):
       self.assertEqual(sumInts(1, 2, 3), 6);
-      self.assertEqual(sumInts(1, 2), 5);
+      self.assertEqual(sumInts(1, 2), 3);
 
